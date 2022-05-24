bc3d5b25bdb39ce8cb413510f9be16fb32357e8d
ViniScardoso <vinicius.cardoso@sptech.school>
2022-05-24 15:30:45 -0300

    progresso no gif de login

--- site/public/js/funcoes.js
+++ site/public/js/funcoes.js
@@ -27,12 +27,12 @@ function limparSessao() {
 // carregamento (loading)
 function aguardar() {
     var divAguardar = document.getElementById("div_aguardar");
-    // divAguardar.style.display = "flex"; 
+    divAguardar.style.display = "flex"; 
 }
 
 function finalizarAguardar(texto) {
     var divAguardar = document.getElementById("div_aguardar");
-    // divAguardar.style.display = "none";
+    divAguardar.style.display = "none";
 
     var divErrosLogin = document.getElementById("div_erros_login");
     if (texto) {
