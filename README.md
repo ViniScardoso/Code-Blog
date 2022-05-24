c03e1701c54f660bf00f3dd524295ff6907694f4
ViniScardoso <vinicius.cardoso@sptech.school>
2022-05-24 00:08:55 -0300

    Cadastro funcionado, gif loading por arrumar

---
+++ README.md
@@ -0,0 +1,3 @@
+# Code Blog
+blog de programação desenvolvido como projeto individual da matéria de pesquisa e inovação
+do primeiro semestre do curso de ciência da computação
---
+++ site/src/database/ScriptTabelasCodeblog.sql
@@ -0,0 +1,30 @@
+drop database CodeBlog;
+create database CodeBlog;
+use CodeBlog;
+
+create table Usuario(
+  idUsuario int primary key auto_increment,
+  nome varchar(45),
+  email varchar(45),
+  senha varchar(45),
+  lingPreferida varchar(45)
+)auto_increment = 1001;
+
+
+create table Post(
+  idPost int primary key auto_increment,
+  titulo varchar(45),
+  descricao varchar(600),
+  linguagem varchar(45),
+  dataPost datetime,
+  fkUsuario int,
+  foreign key(fkUsuario) references Usuario(idUsuario)
+  )auto_increment = 10001;
+  
+  
+create table Resposta(
+  idResposta int primary key auto_increment,
+  descricao varchar(600),
+  fkPost int,
+  foreign key(fkPost) references Post(idPost)
+);
